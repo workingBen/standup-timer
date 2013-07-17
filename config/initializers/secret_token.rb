2183d0e3a2eba65f9903cc2fde915d9d0fc90694
Ben Pellow <bpellow@walmartlabs.com>
2013-07-17 13:05:32 -0700

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StandupTimer::Application.config.secret_key_base = '4ca97eb38e54e717126b43517e0dcb9cfab01e1f24a50132faa2667a95c9fa9c777d12a4e70110efbc9a1c38c24daf878162e514e5eae16bba9527645a1f116b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StandupTimer::Application.config.session_store :cookie_store, key: '_standup-timer_session'
