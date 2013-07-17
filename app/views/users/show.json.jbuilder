f8f314c80394515d4238aaee6dea8f0c265b75c3
Ben Pellow <bpellow@walmartlabs.com>
2013-07-17 13:19:28 -0700

    scaffold users

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :name, :team_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130717201351) do
+ActiveRecord::Schema.define(version: 20130717201909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "teams", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.integer  "team_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
